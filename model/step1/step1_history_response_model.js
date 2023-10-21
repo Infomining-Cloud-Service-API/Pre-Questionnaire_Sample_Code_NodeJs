d25716d7bed1bf39440fb94b3b813ec27b222342
SangYul Kim <tkddbf7348@naver.com>
2023-10-21 18:23:45 +0900

    오타 수정

--- model/step1/step1_history_response_model.js
+++ model/step1/step1_history_response_model.js
@@ -1,8 +1,8 @@
 class Step1HistoryResponseModel {
     constructor(data) {
         this.report_id = data.report_id; // Identifier of the report
-        this.step1_question_id = data.step1_question_id; // Step2 identifier of the question
-        this.step1_selection_id = data.step1_selection_id; // Step2 identifier of the selection
+        this.step1_question_id = data.step1_question_id; // Step1 identifier of the question
+        this.step1_selection_id = data.step1_selection_id; // Step1 identifier of the selection
         this.step1_input_text = data.step1_input_text; // if Subjective Answer, response content
         this.step1_reg_date = data.step1_reg_date; // response date
     }
